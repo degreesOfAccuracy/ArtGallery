320177fbdfe1dd4ca512cefc2396ca32b5de3678
Oscar Sowerbutts <oscarsowerbutts@gmail.com>
2024-10-27 13:18:31 +0000

    Added docstrings. Started implementation of a table using Tkinter. Added read functions to the db interface.

--- database/interface.py
+++ database/interface.py
@@ -3,6 +3,7 @@ import sqlite3
 DATABASE_PATH = "database/database.db"
 
 def execute_script(script_path):
+    "Executes a script of SQL commands"
     with open(script_path) as script_file:
         script = script_file.read()
     con = sqlite3.connect(DATABASE_PATH)
@@ -11,6 +12,7 @@ def execute_script(script_path):
     con.commit()
 
 def write_artist(artist_name: str, street: str, town: str, county: str, postcode: str):
+    "Adds an artist to the artists table"
     con = sqlite3.connect(DATABASE_PATH)
     cur = con.cursor()
     cur.execute(
@@ -19,7 +21,15 @@ def write_artist(artist_name: str, street: str, town: str, county: str, postcode
     )
     con.commit()
 
+def read_artists(cols = "artist_name, street, town, county, postcode"):
+    "Reads the specified columns from all the rows in the artists table"
+    con = sqlite3.connect(DATABASE_PATH)
+    cur = con.cursor()
+    cur.execute(f"SELECT {cols} FROM artists")
+    return cur.fetchall()
+
 def write_artwork(artist_id: int, title: str, medium: str, price: float):
+    "Adds an artwork to the artwork table"
     con = sqlite3.connect(DATABASE_PATH)
     cur = con.cursor()
     cur.execute(
@@ -27,3 +37,20 @@ def write_artwork(artist_id: int, title: str, medium: str, price: float):
         (artist_id, title, medium, price)
     )
     con.commit()
+
+def read_artworks(cols = "artist_id, title, medium, price", convert_ids = True):
+    "Reads the specified columns from all the rows in the artworks table"
+    con = sqlite3.connect(DATABASE_PATH)
+    cur = con.cursor()
+    cur.execute(f"SELECT {cols} FROM artworks")
+    artworks = cur.fetchall()
+    if convert_ids:
+        # find the index of the artist_id column
+        artist_id_index = [col.strip() for col in cols.split(",")].index("artist_id")
+        # get the artist names, then replace the ids with the names
+        cur.execute("SELECT id, artist_name FROM artists")
+        artist_names = dict(cur.fetchall())
+        for i in range(len(artworks)):
+            artist_id = artworks[i][artist_id_index]
+            artworks[i][artist_id_index] = artist_names[artist_id]
+    return artworks
--- main.py
+++ main.py
@@ -1,9 +1,39 @@
 import tkinter as tk
 import database.interface as db
 
+class Table():
+    def __init__(self, headers: list[str], height: int):
+        self.headers = headers
+        self.width = len(headers)
+        self.height = height
+        self.rows = []
+
+    def populate(self, rows: list[tuple]):
+        "Replace the current rows"
+        if any([len(row) != self.width for row in rows]):
+            raise Exception(f"Row was not the expected width of {self.width}") 
+        else:
+            self.rows = rows
+
+    def append(self, row: tuple):
+        "Add to the end of the current rows"
+        if len(row) != self.width:
+            raise Exception(f"Row was not the expected width of {self.width}") 
+        else:
+            self.rows.append(row)
+    
+    def empty(self):
+        "Remove every row"
+        self.rows = []
+
+    def pop(self, index = -1):
+        "Remove and return the row at a specified index, defaults to -1"
+        return self.rows.pop(index)
+
 # start a tkinter window
 root = tk.Tk()
 root.minsize(800, 600)
+root.title("Oscar's Art Gallery Challenge")
 
 # configure rows and columns
 for row_n in range(20):
@@ -11,7 +41,11 @@ for row_n in range(20):
 for col_n in range(15):
     root.columnconfigure(col_n, weight = 1)
 
+# create title at top of window
 title = tk.Label(root, text = "ArtDB", font = ("Calibri", 24), justify = "center")
 title.grid(row = 0, column = 0, sticky = "NSEW", columnspan = 20)
+
+artists_table = Table(["Name", "Street", "Town", "County", "Postcode"], 10)
+artists_table.populate(db.read_artists())
 
 root.mainloop()
